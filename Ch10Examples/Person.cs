d130443cedf26c5712ce863972e6eacb542d687f
d-german <facmet107@gmail.com>
2018-08-04 17:44:04 -0500

    made some changes

---
+++ Ch10Examples/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch10Examples
+{
+   public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        static public int Count { get; private set; } = 0;
+        public Person(string firstName, string lastName):this()
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public Person()
+        {
+            Person.Increment();
+        }
+
+        private static void Increment()
+        {
+            Count++;
+        }
+
+    }
+}
---
+++ Ch10Tests/PersonTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Ch10Examples;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ch10Tests
+{
+    [TestClass]
+    public class PersonTest
+    {
+        [TestMethod]
+        public void PersonConstructorTest()
+        {
+            var p1 = new Person();
+            var p2 = new Person("Mary", "Jane");
+            Assert.AreEqual(2, Person.Count);
+        }
+    }
+}
